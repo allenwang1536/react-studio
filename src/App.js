4f749216ad4c7262ad4044d7b154761c0519ed6d
Allen Wang <allen_wang1@brown.edu>
2024-03-14 19:18:35 -0400

    react-studio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import BakeryItem from "./components/BakeryItem";
 import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
@@ -12,18 +13,35 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [total, setTotal] = useState(0);
+  const [cart, setCart] = useState({});
+
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+      <div className="bakery">
+        <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+        <div className="bakery-data">
+          {bakeryData.map(
+            (
+              item,
+              index // TODO: map bakeryData to BakeryItem components
+            ) => (
+              <BakeryItem name={item.name} description={item.description} price={item.price} image={item.image} setTotal={setTotal} setCart={setCart} cart={cart}/> // replace with BakeryItem component
+            )
+          )}
+        </div>
+       
       </div>
+      <div className="cart">
+          <h2>My Cart</h2>
+          <p>Total: {total}</p>
+          {Object.keys(cart).map(item => 
+            <p>
+              {cart[item]}x {item}
+            </p>
+            
+          )}
+        </div>
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,52 @@
 // TODO: create a component that displays a single bakery item
+
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function BakeryItem({ name, description, price, image, setTotal, cart, setCart }) {
+
+  const onClick = () => {
+    setTotal(prevTotal => parseFloat((prevTotal + price).toFixed(2)))
+    if (cart[name]) {
+      setCart({
+        ...cart,
+        [name]: cart[name] + 1,
+      })
+    } else {
+      setCart({
+        ...cart,
+        [name]: 1
+      })
+    }
+  }
+
+  return (
+    <Card sx={{ maxWidth: 345, flexGrow: 1, margin: '0 2rem 0'}}>
+      <CardMedia
+        component="img"
+        alt={name}
+        height="140"
+        image={image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="body2" color="text.secondary">
+            ${price}
+        </Typography>
+        <Button size="small" onClick={onClick}>Add to Cart</Button>
+      </CardActions>
+    </Card>
+  )
+}
